3de6dc1f8a259a2006499d9b4661293cedd9f7b0
Artem Liaschenko <playfootball826@gmail.com>
2023-08-26 22:21:59 +0300

    ub

--- controllers/pet/deletePet.js
+++ controllers/pet/deletePet.js
@@ -5,6 +5,10 @@ const deletePet = async (req, res) => {
   const { _id: userId } = req.user;
   const { petId } = req.params;
 
+  if (petId) {
+    throw HttpError(404, 'Not found');
+  }
+
   // Удаляем питомца по _owner и petId
   const deletedPet = await Pet.findOneAndDelete({
     _id: petId,
